23c299aca1f7a84648800914cc46e600329158c2
Philip A. Stevenson <mail@philstevenson.tech>
2020-01-07 13:14:13 +0000

    change directory in script [ch746]

--- manual-bootstrap.sh
+++ manual-bootstrap.sh
@@ -16,5 +16,5 @@ fi
 
 echo "[INFO] Clone bootstrap repo"
 sudo -H -n -u $ADMIN_USER bash -c 'cd ~ && rm -rf bootstrap-macos; git clone https://github.com/clear-ai/bootstrap-macos.git'
-
-sudo -H -n -u $ADMIN_USER bash -c '~/bootstrap-macos/bootstrap.sh'
+cd ~/bootstrap-macos
+sudo -H -n -u $ADMIN_USER bash -c 'bootstrap.sh'
